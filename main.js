0a3f748e28659f6d11a6f1aeec3bf419fa389a0f
Norselark <norselark@vivaldi.net>
2020-04-14 17:37:03 +0200

    Fnuker med yew 0.12

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import init, { run_app } from './pkg/yew_test.js';
 
 async function main() {
-   await init('/pkg/yew_test.wasm');
+   await init('/pkg/yew_test_bg.wasm');
    run_app();
 }
 main()
