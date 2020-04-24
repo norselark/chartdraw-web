4469bc27a3f1c237391b7fa2ac7e1dabac149790
Norselark <norselark@vivaldi.net>
2020-04-24 12:16:03 +0200

    Fix centre disk of aspects

--- src/components/canvas_area.rs
+++ src/components/canvas_area.rs
@@ -14,7 +14,8 @@ pub struct CanvasArea {
     // link: ComponentLink<Self>,
     harmonic_cycle: HarmonicCycle,
     positions: Positions,
-    start_of_zodiac: f64,
+    zodiac_start: f64,
+    cycle_offset: f64,
     aspect: bool,
 }
 
@@ -30,21 +31,32 @@ impl Component for CanvasArea {
     type Properties = Props;
 
     fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        let start_of_zodiac = (180. - props.positions.ascendant()) % 360.;
+        let zodiac_start = (props.positions.ascendant() - 180.) % 360.;
+        let cycle_offset = 30.
+            * match props.harmonic_cycle {
+                HarmonicCycle::Cycle(n) => f64::from(n) - 1.,
+                _ => 0.,
+            };
         Self {
             // link,
             harmonic_cycle: props.harmonic_cycle,
             positions: props.positions,
-            start_of_zodiac,
+            zodiac_start,
+            cycle_offset,
             aspect: props.aspect,
         }
     }
 
     fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.cycle_offset = 30.
+            * match props.harmonic_cycle {
+                HarmonicCycle::Cycle(n) => f64::from(n) - 1.,
+                _ => 0.,
+            };
         self.harmonic_cycle = props.harmonic_cycle;
         self.positions = props.positions;
         self.aspect = props.aspect;
-        self.start_of_zodiac = (180. - self.positions.ascendant()) % 360.;
+        self.zodiac_start = (self.positions.ascendant() - 180.) % 360.;
         true
     }
 
@@ -53,13 +65,6 @@ impl Component for CanvasArea {
     }
 
     fn view(&self) -> Html {
-        let cycleoffset = 30.
-            * match self.harmonic_cycle {
-                HarmonicCycle::Cycle(n) => f64::from(n) - 1.,
-                _ => 0.,
-            };
-        let start_of_zodiac = self.start_of_zodiac - cycleoffset;
-
         html! {
             <svg
                 version="1.1" baseProfile="full"
@@ -74,13 +79,14 @@ impl Component for CanvasArea {
                 <circle r="0.703" stroke="white" fill="#5555ff" />
                 { house_sectors() }
                 // Blue semicircle under horizon
-                <g transform=format!("rotate({})", cycleoffset)>
-                    <path d="M -0.703 0 A 0.703 0.703 0 0 0 0.703 0" fill="#0000aa" stroke="white" />
+                <g transform=format!("rotate({})", self.cycle_offset)>
+                    <path d="M -0.703 0 A 0.703 0.703 0 0 0 0.703 0"
+                        fill="#0000aa" stroke="white" />
                 </g>
-                <g transform=format!("rotate({})", -start_of_zodiac)>
+                <g transform=format!("rotate({})", self.zodiac_start + self.cycle_offset)>
                     { five_deg_lines() }
-                    { zodiac_sectors(start_of_zodiac) }
-                    { planet_markers(self.positions.planets(), start_of_zodiac) }
+                    { self.zodiac_sectors() }
+                    { self.planet_markers() }
                 </g>
                 <circle r="0.883" stroke="white" fill="transparent" />
                 <g stroke="#00aaaa" fill="transparent">
@@ -88,14 +94,21 @@ impl Component for CanvasArea {
                     <circle r="1.043" />
                 </g>
                 // Ascendant arrow
-                <g transform=format!("rotate({})", cycleoffset) stroke="white">
-                    <path d="M 0.703 0 H 0.219 M -0.219 0 H -0.703 M -0.863 0 H -0.9" stroke="white" />
-                    <path d="M -0.9 0.01 h -0.02 l -0.03 -0.01 l 0.03 -0.01 h 0.02 z " fill="white" />
+                <g transform=format!("rotate({})", self.cycle_offset) stroke="white">
+                    <path d="M 0.703 0 H 0.219 M -0.219 0 H -0.703 M -0.863 0 H -0.9"
+                        stroke="white" />
+                    <path d="M -0.9 0.01 h -0.02 l -0.03 -0.01 l 0.03 -0.01 h 0.02 z "
+                        fill="white" />
                 </g>
                 // Descendant arrow
-                <g transform=format!("rotate({})", -start_of_zodiac - self.positions.descendant()) stroke="white">
-                    <path d="M 0.703 0 H 0.219 M -0.219 0 H -0.703 M 0.863 0 H 0.9" stroke="white" />
-                    <path d="M 0.9 0.01 h 0.02 l 0.03 -0.01 l -0.03 -0.01 h -0.02 z " stroke="white" fill="black" />
+                <g transform=format!(
+                    "rotate({})",
+                    self.zodiac_start - self.positions.descendant() + self.cycle_offset
+                ) stroke="white">
+                    <path d="M 0.703 0 H 0.219 M -0.219 0 H -0.703 M 0.863 0 H 0.9"
+                        stroke="white" />
+                    <path d="M 0.9 0.01 h 0.02 l 0.03 -0.01 l -0.03 -0.01 h -0.02 z "
+                        stroke="white" fill="black" />
                 </g>
                 // Centre disk
                 {
@@ -122,15 +135,15 @@ impl Component for CanvasArea {
 
 impl CanvasArea {
     fn mini_horizon(&self) -> Html {
-        let sun_transform = format!("rotate({})", -self.start_of_zodiac - self.positions.sun());
-        let moon_transform = format!("rotate({})", -self.start_of_zodiac - self.positions.moon());
+        let sun_transform = format!("rotate({})", self.zodiac_start - self.positions.sun());
+        let moon_transform = format!("rotate({})", self.zodiac_start - self.positions.moon());
         html! {
             <>
                 <circle r="0.219" stroke="white" fill="#5555ff" />
                 <path d="M -0.219 0 A 0.219 0.219 0 0 0 0.219 0" fill="#0000aa" stroke="white" />
                 <path d="M -0.219 0 H 0.219 " stroke="white" />
                 <path d="M -0.219 0.01 h -0.02 l -0.03 -0.01 l 0.03 -0.01 h 0.02 z " stroke="white" fill="white" />
-                <g transform=format!("rotate({})", -self.start_of_zodiac - self.positions.descendant()) stroke="white">
+                <g transform=format!("rotate({})", self.zodiac_start - self.positions.descendant()) stroke="white">
                     <path d="M -0.219 0 H 0.219 " />
                     <path d="M 0.219 0.01 h 0.02 l 0.03 -0.01 l -0.03 -0.01 h -0.02 z " fill="black" />
                 </g>
@@ -147,19 +160,28 @@ impl CanvasArea {
             .planets_without_node()
             .iter()
             .tuple_combinations()
-            .filter_map(|(&a, &b)| 
-                match aspect::aspect(a, b, 8.0) {
-                    Some(asp) if matches!(asp.aspect_type, aspect::AspectType::Zero) => None,
-                    Some(asp) if matches!(asp.aspect_type, aspect::AspectType::Thirty) => None,
-                    Some(asp) => Some((a, b, asp)),
-                    None => None,
-                } 
-            );
-        let rotation = format!("rotate({})", -self.positions.descendant());
+            .filter_map(|(&a, &b)| match aspect::aspect(a, b, 8.0) {
+                Some(asp)
+                    if matches!(
+                        asp.aspect_type,
+                        aspect::AspectType::Zero | aspect::AspectType::Thirty
+                    ) =>
+                {
+                    None
+                }
+                Some(asp) => Some((a, b, asp)),
+                None => None,
+            });
+        let asc_rot = format!("rotate({})", self.cycle_offset);
+        let desc_rot = format!(
+            "rotate({})",
+            self.zodiac_start - self.positions.descendant() + self.cycle_offset
+        );
+        let cycle_rot = format!("rotate({})", self.cycle_offset);
         html! {
             <>
             <circle r="0.656" fill="white" />
-            <g transform=format!("rotate({})", -self.start_of_zodiac)>
+            <g transform=format!("rotate({})", self.zodiac_start + self.cycle_offset)>
                 { for aspect_pairs.map(|(a, b, aspect)| {
                     let stroke = match aspect.aspect_type {
                         aspect::AspectType::Ninety | aspect::AspectType::OneEighty => "#aa0000",
@@ -170,13 +192,62 @@ impl CanvasArea {
                         <path d=chord_path(0.656, a, b) stroke=stroke stroke-width=width />
                     }
                 } ) }
-                <circle r="0.04" fill="#5555ff" stroke="black" />
-                <path d="M -0.04 0 A 0.04 0.04 0 0 0 0.04 0" fill="#0000aa" stroke="black" />
-                <path d="M -0.04 0 H 0.04" stroke="white" transform=rotation/>
             </g>
-            <path d="M -0.4 0 H 0.4" stroke="white" />
+            <circle r="0.04" fill="#5555ff" stroke="black" />
+            <path d="M -0.04 0 A 0.04 0.04 0 0 0 0.04 0" fill="#0000aa" stroke="black" transform=cycle_rot />
+            <path d="M -0.04 0 H 0.04" stroke="white" transform=desc_rot />
+            <path d="M -0.04 0 H 0.04" stroke="white" transform=asc_rot />
             <circle r="0.012" fill="#black" stroke="#55ffff" />
             </>
+        }
+    }
+
+    /// `start_of_zodiac` is passed in order to flip the glyphs right side up
+    fn zodiac_sectors(&self) -> Html {
+        let sector = |offset| {
+            let angle = (30 * offset) as f64;
+            let rotation = format!("rotate({})", -angle);
+            let text_trans = format!(
+                "rotate(-15) translate(0.98, 0) rotate({}) scale(0.008)",
+                angle + 15. - self.zodiac_start - self.cycle_offset
+            );
+            let glyph = ZODIAC_GLYPHS[offset];
+            html! {
+                <g transform=rotation>
+                    <path d="M 0.930 0 L 1.043 0" stroke="#00aaaa" />
+                    <text fill="#00aaaa" transform=text_trans>
+                        { glyph }
+                    </text>
+                </g>
+            }
+        };
+        html! {
+            <g>{ for (0..12).map(sector) }</g>
+        }
+    }
+
+    fn planet_markers(&self) -> Html {
+        let optimized_position = optimize::optimize(self.positions.planets());
+        html! {
+            <g>
+                { for self.positions.planets().iter().enumerate().map(|(i, a)| {
+                    let delta = optimized_position[i] - a;
+                    let text_trans = format!(
+                        "rotate({}) translate(0.775, 0) rotate({}) scale(0.008)",
+                        -delta,
+                        a + delta - self.zodiac_start - self.cycle_offset);
+                    let glyph = PLANET_GLYPHS[i];
+                    html! {
+                        <g transform=format!("rotate({})", -a) stroke-width="0.005">
+                            <circle cx="0.684" r="0.012" stroke="white" fill="transparent" />
+                            <circle cx="0.902" r="0.015" fill="white" />
+                            <text fill="black" transform=text_trans>
+                                { glyph }
+                            </text>
+                        </g>
+                    }
+                } ) }
+            </g>
         }
     }
 }
@@ -203,52 +274,6 @@ fn five_deg_lines() -> Html {
                     <path d="M 0.863 0 L 0.883 0" transform=rotation />
                 }
             }) }
-        </g>
-    }
-}
-
-/// `start_of_zodiac` is passed in order to flip the glyphs right side up
-fn zodiac_sectors(start_of_zodiac: f64) -> Html {
-    let sector = |offset| {
-        let angle = (30 * offset) as f64;
-        let rotation = format!("rotate({})", -angle);
-        let text_trans = format!(
-            "rotate(-15) translate(0.98, 0) rotate({}) scale(0.008)",
-            angle + 15. + start_of_zodiac
-        );
-        let glyph = ZODIAC_GLYPHS[offset];
-        html! {
-            <g transform=rotation>
-                <path d="M 0.930 0 L 1.043 0" stroke="#00aaaa" />
-                <text fill="#00aaaa" transform=text_trans>
-                    { glyph }
-                </text>
-            </g>
-        }
-    };
-    html! {
-        <g>{ for (0..12).map(sector) }</g>
-    }
-}
-
-fn planet_markers(positions: &[f64], start_of_zodiac: f64) -> Html {
-    let optimized_position = optimize::optimize(positions);
-    html! {
-        <g>
-            { for positions.iter().enumerate().map(|(i, a)| {
-                let delta = optimized_position[i] - a;
-                let text_trans = format!("rotate({}) translate(0.775, 0) rotate({}) scale(0.008)", -delta, a + delta + start_of_zodiac);
-                let glyph = PLANET_GLYPHS[i];
-                html! {
-                    <g transform=format!("rotate({})", -a) stroke-width="0.005">
-                        <circle cx="0.684" r="0.012" stroke="white" fill="transparent" />
-                        <circle cx="0.902" r="0.015" fill="white" />
-                        <text fill="black" transform=text_trans>
-                            { glyph }
-                        </text>
-                    </g>
-                }
-            } ) }
         </g>
     }
 }
